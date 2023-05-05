ddf5fd2e09e721143755a32d5849219c3a938dd4
thejoun <the.jonu@gmail.com>
2023-05-05 13:55:18 +0200

    it works!

---
+++ BFW/Assets/Scripts/Game/MyCounter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Context;
+using Dtos;
+using Helpers;
+using Interfaces;
+using Nethereum.Contracts;
+using Nethereum.Hex.HexTypes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game
+{
+    public class MyCounter : MonoBehaviour
+    {
+        [SerializeReference] private IContract myComponent;
+        
+        private INode Node => EditorContext.Instance.Node;
+        private IAccount Account => EditorContext.Instance.Account;
+
+        [Button]
+        private void Increment()
+        {
+            var web3 = Web3Helper.CreateWeb3(Node, Account);
+            var contract = web3.Eth.GetContract(myComponent.Abi, myComponent.Address);
+            var function = contract.GetFunction("change");
+            
+            var gas = new HexBigInteger(100000);
+            
+            var task = function.SendTransactionAndWaitForReceiptAsync(Account.Address, gas, 
+                null, null, 
+                1);
+            task.Wait();
+            
+            Debug.Log("Increment success");
+        }
+
+        [Button]
+        private void Check()
+        {
+            var web3 = Web3Helper.CreateWeb3(Node, Account);
+            var contract = web3.Eth.GetContract(myComponent.Abi, myComponent.Address);
+            var function = contract.GetFunction("check");
+            
+            var task = function.CallAsync<int>();
+            task.Wait();
+            var result = task.Result;
+
+            Debug.Log($"Counter is {result}");
+        }
+    }
+}
--- game/packages/contracts/src/components/MyComponent.sol
+++ game/packages/contracts/src/components/MyComponent.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+import "std-contracts/components/Uint32Component.sol";
 
-contract MyComponent
+uint256 constant ID = uint256(keccak256("component.My"));
+
+contract MyComponent is Uint32Component
 {
   uint32 public value;
+
+  constructor(address world) Uint32Component(world, ID) 
+  {
+
+  }
 
   function change(uint32 delta) public {
     value += delta;
